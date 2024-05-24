431c2ec323869379bb478751ccd189e2dfa24513
Yogeshkodes <yogesh.cr712@gmail.com>
2024-05-24 12:43:34 +0530

    accordian complete

--- Projects/src/Components/1.Accordian/index.jsx
+++ Projects/src/Components/1.Accordian/index.jsx
@@ -3,31 +3,47 @@ import data from "../data";
 import "./style.css";
 
 function Accordion() {
-  const [selectedSet, setSelectedSet] = useState(null);
+  const [selectedIds, setSelectedIds] = useState([]);
+  const [allowMultiple, setAllowMultiple] = useState(false);
 
-  function handleClick(getId) {
-    setSelectedSet(getId === selectedSet ? null : getId);
-  }
-  const datas = data;
+  const toggleSelection = (id) => {
+    if (allowMultiple) {
+      setSelectedIds((prevSelectedIds) =>
+        prevSelectedIds.includes(id)
+          ? prevSelectedIds.filter((selectedId) => selectedId !== id)
+          : [...prevSelectedIds, id]
+      );
+    } else {
+      setSelectedIds((prevSelectedIds) =>
+        prevSelectedIds.includes(id) ? [] : [id]
+      );
+    }
+  };
+
+  const toggleAllowMultiple = () => {
+    setAllowMultiple((prevAllowMultiple) => !prevAllowMultiple);
+  };
+
   return (
     <div className="wrapper">
-      {datas.map((data, index) => {
-        return (
+      <button onClick={toggleAllowMultiple} className="button">
+        {allowMultiple ? "Disable" : "Enable"} multiple selection
+      </button>
+      {data.map((item, index) => (
+        <div className="accordian" key={index}>
           <div
-            onClick={() => handleClick(data.id)}
-            className="accordian"
-            key={index}
+            className="item"
+            onClick={() => toggleSelection(item.id)}
+            style={{
+              backgroundColor: selectedIds.includes(item.id) ? "#f0f0f0" : "",
+            }}
           >
-            <div className="item">
-              <div>{data.question}</div>
-              <div>+</div>
-            </div>
-            <div>
-              {selectedSet === data.id ? <div>{data.answer}</div> : null}{" "}
-            </div>
+            <div>{item.question}</div>
+            <div>{selectedIds.includes(item.id) ? "-" : "+"}</div>
           </div>
-        );
-      })}
+          {selectedIds.includes(item.id) && <div>{item.answer}</div>}
+        </div>
+      ))}
     </div>
   );
 }
